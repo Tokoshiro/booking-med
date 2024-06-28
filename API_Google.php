29febaedd501474e54e20a691c0b52754276e336
Tokoshiro <dgarmendia69@gmail.com>
2024-06-27 19:48:57 -0500

    api google

---
+++ API_Google.php
@@ -0,0 +1,10 @@
+<iframe
+  width="600"
+  height="450"
+  style="border:0"
+  loading="lazy"
+  allowfullscreen
+  referrerpolicy="no-referrer-when-downgrade"
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA6u7UZdiNz3rkAZCsO-nSzr2Qgg7I0sLc
+    &q=Space+Needle,Seattle+WA">
+</iframe>
