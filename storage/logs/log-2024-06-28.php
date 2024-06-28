c586f2f331e32cce23a6584759f1433a34a661c1
Tokoshiro <dgarmendia69@gmail.com>
2024-06-28 16:04:52 -0500

    act

---
+++ storage/logs/log-2024-06-28.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-06-28 22:08:31 --> Severity: Warning --> Trying to access array offset on value of type bool C:\xampp\htdocs\booking-med\system\libraries\Cache\drivers\Cache_file.php 277
