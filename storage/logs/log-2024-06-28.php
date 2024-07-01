508446297605110e0e9a3f93dccf60e879405f48
Tokoshiro <dgarmendia69@gmail.com>
2024-07-01 18:35:15 -0500

    error logs

--- storage/logs/log-2024-06-28.php
+++ storage/logs/log-2024-06-28.php
@@ -1,3 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
+<<<<<<< Updated upstream
 ERROR - 2024-06-28 22:08:31 --> Severity: Warning --> Trying to access array offset on value of type bool C:\xampp\htdocs\booking-med\system\libraries\Cache\drivers\Cache_file.php 277
+=======
+ERROR - 2024-06-28 22:28:41 --> Severity: error --> Exception: No se puede establecer una conexión ya que el equipo de destino denegó expresamente dicha conexión C:\xampp\htdocs\booking-med\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-06-28 22:28:43 --> Severity: error --> Exception: No se puede establecer una conexión ya que el equipo de destino denegó expresamente dicha conexión C:\xampp\htdocs\booking-med\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-06-28 22:29:12 --> Severity: error --> Exception: No se puede establecer una conexión ya que el equipo de destino denegó expresamente dicha conexión C:\xampp\htdocs\booking-med\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-06-28 22:29:19 --> Severity: error --> Exception: No se puede establecer una conexión ya que el equipo de destino denegó expresamente dicha conexión C:\xampp\htdocs\booking-med\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-06-28 22:42:39 --> Severity: error --> Exception: No se puede establecer una conexión ya que el equipo de destino denegó expresamente dicha conexión C:\xampp\htdocs\booking-med\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-06-28 22:43:32 --> Severity: error --> Exception: No se puede establecer una conexión ya que el equipo de destino denegó expresamente dicha conexión C:\xampp\htdocs\booking-med\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-06-28 22:44:03 --> Severity: Warning --> Trying to access array offset on value of type bool C:\xampp\htdocs\booking-med\system\libraries\Cache\drivers\Cache_file.php 277
+>>>>>>> Stashed changes
